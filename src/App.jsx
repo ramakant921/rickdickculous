31c7f91d09666655bc1e726a2819554610eeea06
zalos <satya123m4@gmail.com>
2026-08-18 23:07:47 +0530

    update: tab thing, scroll indicator and curved text in Hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from "./About";
 function App() {
   return (
     <>
-  <PixelEffect />
+    <PixelEffect />
     <Hero />
     {/* <About /> */}
     {/* <div className="h-screen grid place-content-center"> */}
