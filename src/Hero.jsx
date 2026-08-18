0921eb0106e1c3ac13d0f530182115a56b999422
zalos <satya123m4@gmail.com>
2026-08-18 19:00:28 +0530

    add: paralax to hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,8 +1,41 @@
 import "./index.css";
+import { useEffect, useRef } from "react";
 import rock1 from "./assets/rock1.png"
 import rock2 from "./assets/rock2.png"
 
 function Hero() {
+  // Trying to code parallax
+  const titleRef = useRef(null);
+  const rock1Ref = useRef(null);
+  const rock2Ref = useRef(null);
+
+  const mouse = {
+    x: 0,
+    y: 0, 
+  }
+
+  useEffect(() => {
+
+    const handleMouse = (e) => {
+      mouse.x = e.clientX;
+      mouse.y = e.clientY;
+
+      // move
+      rock1Ref.current.style.transform =
+        `translate(${mouse.x * 0.05}px, ${mouse.y *0.05}px)`;
+      rock2Ref.current.style.transform =
+        `translate(${mouse.x * 0.02}px, ${mouse.y *0.02}px)`;
+      titleRef.current.style.transform =
+        `translate(${mouse.x * 0.01}px, ${mouse.y *0.01}px)`;
+    }
+
+    window.addEventListener("mousemove", handleMouse);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouse);
+    }
+  }, []);
+
   return (
     <>
     <div id="" className="">
@@ -15,12 +48,12 @@ function Hero() {
     <li>About</li>
     </ul>
 
-    <h1 className="z-0 text-center text-[12vw] font-bold">PORTFOLIO</h1>
-    <img src={rock2}
-    className="z-1 w-[12vw] absolute top-[25%] left-[20%]"
+    <h1 ref={titleRef} className="smoothIt z-0 text-center text-[12vw] font-bold">PORTFOLIO</h1>
+    <img ref={rock2Ref} src={rock2}
+    className="smoothIt z-1 w-[12vw] absolute top-[25%] left-[20%]"
     />
-    <img src={rock1}
-    className="z-3 w-[20vw] absolute top-[43%] right-[18%]"
+    <img ref={rock1Ref} src={rock1}
+    className="smoothIt z-3 w-[20vw] absolute top-[43%] right-[18%]"
     />
     </div>
 
